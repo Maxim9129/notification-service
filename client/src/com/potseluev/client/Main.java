6f22f56ae77b6b4041f979fb877a622fd3178c56
Maksim Potseluev <maksim.potseluev@mail.ru>
2015-07-15 19:06:12 +0300

    Initial commit with client and server

---
+++ client/src/com/potseluev/client/Main.java
@@ -0,0 +1,69 @@
+package com.potseluev.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+/**
+ * Created by Рустам on 15.07.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try (
+            Socket socket = new Socket("localhost", 4444);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        )
+        {
+            HashMap<String, String> params = new HashMap<>();
+            params.put("email", "rustam2890@gmail.com");
+            params.put("message", "Hello World");
+
+            printWriter.println(Main.encodeCommand(params));
+            System.out.println(bufferedReader.readLine());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encodeCommand(HashMap<String, String> params) {
+        String result = "";
+
+        boolean first = true;
+        for(String key : params.keySet()) {
+            try {
+                result += (!first ? "&" : "") + key + "=" + URLEncoder.encode(params.get(key), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+            }
+
+            first = false;
+        }
+
+        return result;
+    }
+
+    public static HashMap<String, String> decodeCommand(String encoded) {
+        HashMap<String, String> params = new HashMap<>();
+
+        String[] parts = encoded.split("&");
+        for (String part: parts) {
+            String[] keyValue = part.split("=");
+            String key = keyValue[0];
+            String value = null;
+            try {
+                value = URLDecoder.decode(keyValue[1], "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+            }
+
+            params.put(key, value);
+        }
+
+        return params;
+    }
+
+
+}
---
+++ server/src/com/potseluev/server/Main.java
@@ -0,0 +1,33 @@
+package com.potseluev.server;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int portNumber = 4444;
+
+        try (
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+
+
+            String line = in.readLine();
+            out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
